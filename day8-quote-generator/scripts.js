f40d2e83d4e2f6416cfb5cc40d08be2733d7308b
lamifaharia <lamifaharia@gmail.com>
2026-08-18 01:40:48 +0600

    random quote generator

---
+++ day8-quote-generator/scripts.js
@@ -0,0 +1,67 @@
+// ==========================
+// Select HTML Elements
+// ==========================
+
+const quoteElement = document.getElementById("quote");
+
+const authorElement = document.getElementById("author");
+
+const newQuoteBtn = document.getElementById("newQuoteBtn");
+
+
+// ==========================
+// Quotes Array
+// ==========================
+
+const quotes = [
+    {
+        text: "The only way to do great work is to love what you do.",
+        author: "Steve Jobs"
+    },
+
+    {
+        text: "Believe you can and you're halfway there.",
+        author: "Theodore Roosevelt"
+    },
+
+    {
+        text: "It always seems impossible until it's done.",
+        author: "Nelson Mandela"
+    },
+
+    {
+        text: "Success is not final, failure is not fatal.",
+        author: "Winston Churchill"
+    },
+
+    {
+        text: "The future depends on what you do today.",
+        author: "Mahatma Gandhi"
+    },
+
+    {
+        text: "Dream big and dare to fail.",
+        author: "Norman Vincent Peale"
+    },
+
+    {
+        text: "Do something today that your future self will thank you for.",
+        author: "Sean Patrick Flanery"
+    },
+
+    {
+        text: "Don't watch the clock; do what it does. Keep going.",
+        author: "Sam Levenson"
+    },
+
+    {
+        text: "Everything you can imagine is real.",
+        author: "Pablo Picasso"
+    },
+
+    {
+        text: "Great things are done by a series of small things brought together.",
+        author: "Vincent van Gogh"
+    }
+];
+
